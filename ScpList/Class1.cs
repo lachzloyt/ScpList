73c63d23dab069c3019956e5e723ffe3c8d17509
formu <formu@DESKTOP-FRC41UP>
2024-10-16 20:05:51 +0300

    Add project files.

---
+++ ScpList/Class1.cs
@@ -0,0 +1,96 @@
+﻿using Exiled.API.Features;
+using Exiled.Loader;
+using MEC;
+using PlayerRoles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using player = Exiled.Events.Handlers.Player;
+using server = Exiled.Events.Handlers.Server;
+
+namespace ScpList
+{
+    public class Class1 : Plugin<Config>
+    {
+
+        public override string Name => "Scplist";
+        public override string Prefix => Name;
+        public override string Author => "Lachzloyt";
+
+
+        public static Class1 Instance;
+        public CoroutineHandle coroutineHandle;
+        public StringBuilder sb;
+        private IEnumerator<float> coroutine;
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            Exiled.Events.Handlers.Server.RoundStarted += OnRoundStarted;
+            Exiled.Events.Handlers.Server.RoundEnded += OnRoundEnded;
+            base.OnEnabled();
+           
+        }
+        public override void OnDisabled()
+        {
+            Instance = null;
+            Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStarted;
+            Exiled.Events.Handlers.Server.RoundEnded -= OnRoundEnded;
+            Timing.KillCoroutines(coroutineHandle);
+            base.OnDisabled();
+        }
+        public void OnRoundStarted()
+        {
+
+            coroutineHandle = Timing.RunCoroutine(UpdateInfo());
+
+
+        }
+        
+        private void OnRoundEnded(Exiled.Events.EventArgs.Server.RoundEndedEventArgs ev)
+        {
+            Timing.KillCoroutines(coroutineHandle);
+        }
+
+        public IEnumerator<float> UpdateInfo()
+        {
+            while (Round.IsStarted)
+            {
+
+                string FirstColorHex = Instance.Config.FirstColorHex; 
+                string SecondColorHex = Instance.Config.SecondColorHex; 
+                string MainColorHex = Instance.Config.MainColorHex; 
+                string TextVar = Instance.Config.TextShow;
+
+                StringBuilder sb = new StringBuilder();
+
+                sb.AppendLine($"<color={MainColorHex}>{TextVar}</color>");  
+               
+               foreach (Player player in Player.List.Where(p => p.Role.Team == Team.SCPs && p.IsAlive))
+                {
+                    sb.Append($"|<size=32><color={FirstColorHex}>{player.Role.Name} </color> :  <color={SecondColorHex}>{Convert.ToInt32(player.Health)}HP</color></size>| " );
+                    
+                }
+
+                string scpInfo = sb.ToString();
+
+                float Showtime = Instance.Config.Showtime;
+                int Showduration = Instance.Config.Showduration;
+                // Display the information to all players
+                foreach (Player player in Player.List)
+                {
+                    Map.Broadcast((ushort)Showduration, scpInfo); // Display for 5 seconds
+                }
+
+                // Wait for 5 seconds before updating the SCP info again
+                yield return Timing.WaitForSeconds(Showtime);
+
+
+            }
+
+        }
+    }
+}
---
+++ ScpList/Config.cs
@@ -0,0 +1,19 @@
+﻿using Exiled.API.Interfaces;
+using Org.BouncyCastle.Utilities.Encoders;
+using UnityEngine;
+
+namespace ScpList
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+        public float Showtime { get; set; } = 30f;
+        public int Showduration { get; set; } = 5;
+        public string FirstColorHex { get; set; } = "#FFFFFF";
+        public string SecondColorHex { get; set; } = "#ff0000";
+        public string MainColorHex { get; set; } = "#ff0000";
+        public string TextShow { get; set; } = "Alive SCPs:";
+
+    }
+}
